ef9e7e5f69bd2978e117fc655ae267d476d332e1
Carlos Diez <cdiezmoran@gmail.com>
2017-04-20 18:48:49 -0700

    Analytics base css

--- app/components/Dashboard/AnalyticsGrid.js
+++ app/components/Dashboard/AnalyticsGrid.js
@@ -4,7 +4,30 @@ export default class AnalyticsGrid extends Component {
   render() {
     return (
       <div className="analytics-grid">
-        <h1>AnalyticsGrid</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="analytics-card">
+                <div className="ac-header">
+                  <span className="ev-title">Sessions</span>
+                </div>
+                <div className="ac-content">
+                  <span className="ev-value">1337</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="analytics-card">
+                <div className="ac-header">
+                  <span className="ev-title">Total Players</span>
+                </div>
+                <div className="ac-content">
+                  <span className="ev-value">57475</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
